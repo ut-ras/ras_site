9d5a492b290d537c2ae831163bdebb6d76baeadb
DhruvNistala <dhruv.nistala@gmail.com>
2024-09-05 10:35:21 -0500

    added igvc github link

--- src/components/pages/committees/IGVC.tsx
+++ src/components/pages/committees/IGVC.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import links from "@/constant/links.json";
 import people from "@/constant/people.json";
 import styles from "@/css/pages/committees/IGVC.module.css";
-import { FaDiscord } from "react-icons/fa6";
+import { FaDiscord, FaGithub } from "react-icons/fa6";
 import Card from "@/components/Card";
 import igvc_track from "@/assets/F22IGVC_Track.png";
 import igvc_robot from "@/assets/F22IGVC_Robot.png";
@@ -13,11 +13,17 @@ export default function IGVC() {
         <main>
             <h1>IGVC Committee</h1>
             <h2>Committee Head: {people.committee_heads[1].name}</h2>
-            <div className={styles.DiscordLink}>
+            <div className={styles.linkAndIcon}>
                 <a href={links.igvc_discord} target="_blank" className={styles.portfolioIcon}>
                     <FaDiscord size={40} />
                 </a>
                 <h3><Link target="_blank" to={links.igvc_discord}>Discord</Link></h3>
+            </div>
+            <div className={styles.linkAndIcon}>
+                <a href={links.igvc_github} target="_blank" className={styles.portfolioIcon}>
+                    <FaGithub size={40} />
+                </a>
+                <h3><Link target="_blank" to={links.igvc_github}>GitHub</Link></h3>
             </div>
             <p>IGVC is a competitive committee whose objective is to build an autonomous vehicle! It must be able to navigate a football field-sized obstacle course under a time limit. The fastest robot to successfully traverse the course wins a grand prize of $3000.</p>
             <div className={styles.trackImageContainer}><img src={igvc_track} alt="IGVC Track" width="35%" height="auto" className={styles.trackImage} /></div>
